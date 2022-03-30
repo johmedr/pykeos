105079f327c850dda8566a0588ca14cff91ad73c
Johan Medrano <jmedrano@lirmm.fr>
2022-03-30 13:10:26 +0200

    updating localized computation of histograms

--- pykeos/tools/_impl/impl.pyx
+++ pykeos/tools/_impl/impl.pyx
@@ -24,27 +24,75 @@ ctypedef np.float64_t FLOAT64TYPE_t
 def _localized_diagline_histogram(
     int n_time, np.ndarray[INT8TYPE_t, ndim=2] recmat):
 
-    cdef int i, j, k, i_start, j_start = 0
+    cdef int i, j, k
     cdef list diagline_list = []
+    cdef np.ndarray[INT32TYPE_t, ndim=1] start_index = np.zeros([n_time - 1], dtype=np.int32)
+    cdef np.ndarray[INT32TYPE_t, ndim=1] diag_length = np.zeros([n_time - 1], dtype=np.int32)
+
+    i, j, k = 0, 0, 0
+
+    # Iterate over rows
     for i in range(n_time):
-        if k != 0:
-            if k > 1:
-                diagline_list.append((i_start, j_start, k))
-            k = 0
-        for j in range(i+1):
-            if recmat[n_time-1-i+j, j] == 1:
-                if k == 0:
-                    i_start = i
-                    j_start = j
-                k += 1
-            elif k != 0:
-                if k > 1:
-                    diagline_list.append((i_start, j_start, k))
-                k = 0
+        # Iterate over diagonals 
+        for k in range(0, n_time-i-1):
+            # If (i, i + diag) is recurrent
+            if recmat[i, i + k + 1] == 1:
+                # If the diag was not a 1-diagonal, change the start index
+                if diag_length[k] == 0: 
+                    start_index[k] = i
+
+                diag_length[k] += 1
+            # Else if (i, i + k) is not recurrent and we were in a 1-diagonal
+            elif diag_length[k] > 0:
+                # Store diagonal 
+                diagline_list.append((start_index[k], start_index[k] + k, diag_length[k])) 
+                diag_length[k] = 0
 
     return diagline_list
 
+def _weight_function(int di, int dj, int scale, int order):
+    cdef float dist = max(abs(di), abs(dj)) / scale
+    if dist < 1:
+        return np.exp(1. - 1. / (1 - dist ** order))
+    else:
+        return 0.
 
+
+def _weighted_diagline_histogram(
+    int n_time, list diaglines, np.ndarray[FLOATTYPE_t, ndim=2] diagline_series,int scale,  int order):
+
+    cdef int i, j, k, i_start, j_start, i_mid, j_mid
+    cdef int di, dj
+    k = 0
+    i_start = 0
+    j_start = 0
+    i_mid = 0
+    j_mid = 0
+    cdef np.ndarray[FLOATTYPE_t, ndim=2] weights = np.zeros([2 * scale + 1, 2 * scale + 1], dtype=np.float)
+    cdef float w = 0
+
+    weights[scale, scale] = _weight_function(0, 0, scale, order)
+
+    for i in range(scale): 
+        for j in range(scale):
+            w = _weight_function(i + 1, j + 1, scale, order)
+
+            weights[scale + i + 1, scale + j + 1] = w
+            weights[scale + i + 1, scale - j - 1] = w
+            weights[scale - i - 1, scale + j + 1] = w
+            weights[scale - i - 1, scale - j - 1] = w
+
+
+    for (i_start, j_start, k) in diaglines:
+        i_mid = int(round((i_start + k) / 2.))
+        j_mid = int(round((j_start + k) / 2.))
+
+        for i in range( max(min(i_mid - scale, j_mid - scale), 0) , min(max(i_mid + scale, j_mid + scale), n_time)):
+            di = i - i_mid
+            dj = i - j_mid
+
+            if max(abs(di), abs(dj)) < scale:
+                diagline_series[i , k] += weights[scale + di, scale + dj]
 
 
 def _localized_vertline_histogram(int n_time, np.ndarray[INT8TYPE_t, ndim=2] recmat):
--- pykeos/tools/recurrence_plot_utils.py
+++ pykeos/tools/recurrence_plot_utils.py
@@ -1,5 +1,5 @@
 from ._impl.impl import _localized_diagline_histogram, \
-    _localized_vertline_histogram, _localized_white_vertline_histogram
+    _localized_vertline_histogram, _localized_white_vertline_histogram, _weighted_diagline_histogram
 import numpy as np
 from scipy.sparse import lil_matrix
 
@@ -21,22 +21,19 @@ def localized_white_vertline_histogram(recmat):
     white_vertline_d = _localized_white_vertline_histogram(n_times, recmat)
     return white_vertline_d
 
-#
-# def weighting_kernel(t, i, j, scale):
-#     return np.exp(- (max(abs(t - i), abs(t - j))/scale) ** 2)
-#
-#
-# def weighted_diagline_histogram(recmat, scale):
-#     recmat = recmat.astype(np.int8)
-#     n_times = recmat.shape[0]
-#     diag_hist = localized_vertline_histogram(recmat)
-#     weighted_histogram = lil_matrix((n_times, int(np.sqrt(2 * n_times**2))))
-#     for t in range(n_times):
-#         for (i, j, k) in diag_hist:
-#             weight = weighting_kernel(t, (i + k) / 2, (j + k) / 2, scale)
-#             weighted_histogram[t, k] += weight
-#
-#     return weighted_histogram
+
+from tqdm import tqdm
+
+def weighted_diagline_histogram(recmat, scale, order=4):
+    recmat = recmat.astype(np.int8)
+    n_times = recmat.shape[0]
+    diag_hist = localized_vertline_histogram(recmat)
+    max_diag_length = int(max(_[2] for _ in diag_hist) + 1)
+    if max_diag_length > n_times:
+        raise RuntimeError(f"max_diag_length ({max_diag_length}) is greater that n_times ({n_times})")
+    weighted_histogram = np.zeros((n_times, max_diag_length), dtype=float)
+    _weighted_diagline_histogram(n_times, diag_hist, weighted_histogram, scale, order)
+    return weighted_histogram
 
 
 
